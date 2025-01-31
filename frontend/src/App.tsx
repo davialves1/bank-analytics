0ee9cfede9df38aa9ca521261867d24da4d25cd3
Alves, Davi (K-DACT/J) <davi.alves@volkswagen.de>
2025-01-31 12:08:33 +0100

    created docker compose and docker files, added nginx to network traffic, added dummy go server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
+const baseURL = import.meta.env.VITE_BACKEND_API;
+
 function App() {
+  const [response, setResponse] = useState<Ping | null>(null);
+
+  const ping = async () => {
+    const res = await fetch(baseURL + '/api/ping');
+    if (res.ok) {
+      const json = await res.json();
+      const time = new Date(json.time).toUTCString();
+      setResponse({ message: json.message, time });
+    }
+  };
+
   return (
     <>
       <h1>Bank Analytics</h1>
+      <button onClick={ping}>Ping</button>
+      {response && (
+        <div>
+          {response.message} - {response.time}
+        </div>
+      )}
     </>
   );
 }
 
 export default App;
+
+type Ping = {
+  message: string;
+  time: string;
+};
--- go-server/cmd/main.go
+++ go-server/cmd/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
-	server.GET("/api", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello World"})
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:5173", "http://frontend"}
+	server.Use(cors.New(corsConfig))
+
+	server.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong", "time": time.Now()})
 	})
-	err := server.Run("localhost:8080")
+	err := server.Run("0.0.0.0:8080")
 	if err != nil {
 		fmt.Println("Server failed to initiate", err)
 	}
